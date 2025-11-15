c9172db2f1f563f8b92282db47eabb68d6e97fcb
EddieO23 <eocampo52@gmail.com>
2025-11-15 13:36:22 -0600

    fixed index.thml cdn link for tailwind. classes work as they should now. successfully fetching the channelInfoResponse from the YoutubeAPI. Now need to assign the response to a variable and render it to the markup.

--- src/Pages/Channel.tsx
+++ src/Pages/Channel.tsx
@@ -1,4 +1,26 @@
+import axios from 'axios';
+import { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+
+const API_KEY = import.meta.env.VITE_API_KEY;
+
 function Channel() {
+  const { channelId } = useParams();
+
+  const fetchChannelInfo = async () => {
+    try {
+      const channelInfoResponse = await axios.get(
+        `https://www.googleapis.com/youtube/v3/channels?key=${API_KEY}&part=snippet,contentDetails,statistics&id=${channelId}`
+      );
+
+      console.log(channelInfoResponse);
+    } catch (error) {}
+  };
+
+  useEffect(() => {
+    fetchChannelInfo();
+  }, []);
+
   return (
     <div className='w-[95%] mx-auto mt-8'>
       <div className='row row-cols-2'>
@@ -7,8 +29,22 @@ function Channel() {
           <div className='w-52 aspect-square rounded-full bg-red-300 mx-auto'></div>
         </div>
         {/* details */}
-        <div className="col-8">
-          
+        <div className='col-8'>
+          <h1 className='text-4xl font-semibold'>Channel title</h1>
+          <div className='flex gap-2 text-neutral-400 text-lg mt-2'>
+            <h2>Custom Url</h2>
+            <h2>Subcount</h2>
+            <h2>Video Count</h2>
+          </div>
+          <div className=''>
+            <p className='w-[600px] line-clamp-3 text-neutral-400 whitespace-pre-line'>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Cupiditate illo expedita aperiam nisi placeat blanditiis
+              exercitationem earum ipsum ad, possimus enim sunt consequatur nam
+              similique culpa provident ea nulla tenetur?
+            </p>
+            <button className='font-semibold'>more...</button>
+          </div>
         </div>
       </div>
     </div>
