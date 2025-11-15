8a019153524bc6e6ffafdef4556557462854c22c
EddieO23 <eocampo52@gmail.com>
2025-11-15 12:51:39 -0600

    started working on the markup skeleton for the channelPage

--- src/Components/Card.tsx
+++ src/Components/Card.tsx
@@ -1,17 +1,18 @@
 import { Link } from 'react-router-dom';
-
 import type { HomeVideoCardType } from '../Utils/Types';
 
 function Card({ data }: { data: HomeVideoCardType }) {
   return (
     <div className='flex flex-col gap-3 pb-3'>
-      {/* thumbanail */}
+      {/* thumbnail */}
       <div className='relative'>
-        {/* <div className='bg-red-300 aspect-[16/9] rounded-xl'></div> */}
-        <Link to={`/watch/${data.videoId}/${data.channelInfo.id}`}>
+        <Link
+          to={`/watch/${data.videoId}/${data.channelInfo.id}`}
+          className='block'
+        >
           <img
-          onClick={}
             src={data.videoThumbnail}
+            alt='Video Thumbnail'
             className='aspect-[16/9] object-cover rounded-xl w-full'
           />
         </Link>
@@ -21,12 +22,14 @@ function Card({ data }: { data: HomeVideoCardType }) {
       </div>
       {/* details */}
       <div className='flex gap-2'>
-        {/* <div className='bg-red-300 aspect-[1/1] rounded-full h-12'></div> */}
-        <img
-          src={data.channelInfo.image}
-          className='aspect-[1/1] rounded-full h-12'
-        />
-        <div className='flex flex-col'>
+        <Link to={`/channel/${data.channelInfo.id}`} className='flex-shrink-0'>
+          <img
+            src={data.channelInfo.image}
+            alt='Channel Logo'
+            className='aspect-square rounded-full h-12 w-12 object-cover'
+          />
+        </Link>
+        <div className='flex flex-col overflow-hidden'>
           <h3 className='text-lg line-clamp-2'>{data.videoTitle}</h3>
           <div className='text-md'>
             <h4>{data.channelInfo.name}</h4>
--- src/Pages/Channel.tsx
+++ src/Pages/Channel.tsx
@@ -1,9 +1,18 @@
 function Channel() {
   return (
-    <div>
-      CHANNEL PAGE
+    <div className='w-[95%] mx-auto mt-8'>
+      <div className='row row-cols-2'>
+        {/* image */}
+        <div className='col-4'>
+          <div className='w-52 aspect-square rounded-full bg-red-300 mx-auto'></div>
+        </div>
+        {/* details */}
+        <div className="col-8">
+          
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Channel
+export default Channel;
