0b253b7c4ff22c672e7ff147c8e6c14fa7f46395
EddieO23 <eocampo52@gmail.com>
2025-11-30 14:40:11 -0600

    added parse playlist info data function to parse file. refactored code for better readability.

---
+++ src/Hooks/usePlaylist.ts
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+
+import type { PlaylistInfoType } from '../Utils/Types';
+import { getPlaylistInfo } from '../Utils/api';
+import { parsePlaylistInfo } from '../Utils/parseData';
+
+export const usePlaylistInfo = () => {
+  const [playlistInfo, setPlaylistInfo] = useState<PlaylistInfoType | null>(
+    null
+  );
+  const [showDescription, setShowDescription] = useState(false);
+
+  const fetchPlaylistData = async (playlistId: string | null) => {
+    const playlistInfoReponse = await getPlaylistInfo(playlistId!);
+    // console.log("playlistInfoReponse", playlistInfoReponse);
+
+    const playlistInfoData = parsePlaylistInfo(playlistInfoReponse);
+
+    console.log('playlistInfo', playlistInfoData);
+    setPlaylistInfo(playlistInfoData);
+  };
+
+  return {
+    playlistInfo,
+    fetchPlaylistData,
+    showDescription,
+    setShowDescription,
+  };
+};
